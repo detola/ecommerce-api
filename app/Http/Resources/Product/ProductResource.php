a02ebe8692848d440d3c344a7e46757e9f4d3790
Detola <bawsemajor@gmail.com>
2019-05-01 16:39:02 +0100

    Mdoified product detail logic

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round($this->price - (($this->discount/100) * $this->price), 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id),
+            ]
         ];
     }
 }
