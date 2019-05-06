837975abd20fdf14cd1097dbff3b8957099f7b99
Detola <bawsemajor@gmail.com>
2019-05-06 08:42:27 +0100

    Product update logic

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -61,7 +61,11 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+
+        return response([
+            $data = new ProductResource($product)
+        ], Response::HTTP_CREATED);
     }
 
     /**
--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = [ 'name', 'description', 'price', 'stock', 'discount'];
+
     public function reviews()
     {
        return $this->hasMany(Review::class);
